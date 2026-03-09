a6dd14773f6f93747f1dde75936c7a0be5c5f990
Ebube2701 <emeremoluebube@gmail.com>
2026-03-09 17:47:54 +0100

    Fix: Updated API URL to HTTPS for Vercel security

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg"
 import MovieCard from './MovieCard';
 
 //3af61b
-const API_URL = "http://www.omdbapi.com?apikey=3af61b";
+const API_URL = "https://www.omdbapi.com?apikey=3af61b";
 
 function App() {
 
